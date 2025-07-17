3d3411f78fdf9d4142e820f633ca7a7e49b74edc
titus672 <titusfreezn@gmail.com>
2025-07-17 15:47:26 -0700

    lsp formatting change

--- connectors.py
+++ connectors.py
@@ -2,6 +2,7 @@
 import requests
 from config import CONFIG
 
+
 def nms_connector(endpoint, action="get"):
     config = CONFIG()
     url = f"https://{config.unms_url}/nms/api/v2.1/{endpoint}"
@@ -9,18 +10,22 @@ def nms_connector(endpoint, action="get"):
     request = requests.request(action, url, headers=headers)
     return request.json()
 
+
 def test_nms_connector():
-    import os, json
+    import os
+    import json
     if os.path.exists("device.json"):
         with open("device.json", "r") as device:
             print("reading data from file")
             return json.load(device)
     else:
-         data = nms_connector("devices?withInterfaces=True&authorized=True")
-         with open("device.json", "w") as f:
-             json.dump(data, f, indent=4)
-         print("writing data to file")
-         return data
+        data = nms_connector("devices?withInterfaces=True&authorized=True")
+        with open("device.json", "w") as f:
+            json.dump(data, f, indent=4)
+        print("writing data to file")
+        return data
+
+
 def main():
     # test connections
     import json
@@ -29,6 +34,7 @@ def main():
         print(json.dumps(devices, indent=4))
     except:
         exit(1)
+
 
 if __name__ == "__main__":
     main()
